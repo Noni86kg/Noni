c2bfc915c12bcaae712236d6d420a9c28a42d65d
Noni86kg <86426225+Noni86kg@users.noreply.github.com>
2021-08-03 21:41:08 +0200

    Add files via upload

---
+++ 9-biznis-scss-flex/index.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Shop</title>
+     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+     <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;600;700&display=swap" rel="stylesheet"> 
+     <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400&display=swap" rel="stylesheet"> 
+     <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<!-- nav -->
+<nav>
+    <div class="log">BUSINESS <br>
+    <p>Marketing & consulting</p>
+</div>
+    <ul>
+        <li><a href="">HOME</a></li>
+        <li><a href="">ABOUT</a></li>
+        <li><a href="">SERVICE</a></li>
+        <li><a href="">TEAM</a></li>
+        <li><button>SIGN UP</button></li>
+        <a href=""><i class="fas fa-search"></i></a>
+    </ul>
+</nav>
+
+<!-- header -->
+<header>
+    <div class="container">
+        <h1>Trusted and<br> professionals advisers</h1>
+        <h3>For your business</h3>
+        <p>Showcase your Profile to the world using online CV builder and Get Hired Faster
+</p>
+        <button>About us</button>
+    </div>
+</header>
+
+<!-- One -->
+<section class="oneTwo container">
+    <div class="left">
+        <p>01</p>
+    </div>
+    <div class="right">
+        <h5>ABOUT US</h5>
+        <h2><span>WE CAN </span> HELP YOUR BUSINESS GROW</h2>
+        <h4>Get your Business in 4 easy steps</h4>
+    </div>
+</section>
+
+<!-- about -->
+<section class="about">
+    <article>
+        <img src="img/about.jpg" alt="about">
+    </article>
+    <article>
+        <h2>EVERYTHING YOU NEED IN ONE SOLUTION</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam officia explicabo architecto maxime porro labore quisquam officiis odit a dignissimos hic, soluta accusamus numquam eum culpa veritatis iure provident quidem recusandae sint voluptas. Amet suscipit nesciunt quas quos velit, repudiandae ea adipisci commodi unde, nam odio modi aperiam, numquam recusandae.</p>
+    </article>
+</section>
+
+<!-- Two -->
+<section class="oneTwo container">
+    <div class="left">
+        <p>02</p>
+    </div>
+    <div class="right">
+        <h5>SERVICES</h5>
+        <h2><span>HOW TO PROVIDE </span>TOOLS THAT HELP ANYONE</h2>
+        <h4>Get your Business in 4 easy steps</h4>
+    </div>
+</section>
+
+<!-- Marketing -->
+<section class="marketing container">
+    <article>
+        <i class="far fa-chart-bar"></i>
+        <h2>Marketing</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque exercitationem quasi tempore quisquam, at vero quod harum ad et nulla, ipsum repellat illum, eum magnam laudantium? Ipsum dolorem ipsam corporis?</p>
+    </article>
+
+    <article>
+        <i class="fas fa-bullseye"></i>
+        <h2>Marketing</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque exercitationem quasi tempore quisquam, at vero quod harum ad et nulla, ipsum repellat illum, eum magnam laudantium? Ipsum dolorem ipsam corporis?</p>
+    </article>
+
+    <article>
+        <i class="fas fa-map-signs"></i>
+        <h2>Marketing</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque exercitationem quasi tempore quisquam, at vero quod harum ad et nulla, ipsum repellat illum, eum magnam laudantium? Ipsum dolorem ipsam corporis?</p>
+    </article>
+
+    <article>
+        <i class="fas fa-filter"></i>
+        <h2>Marketing</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque exercitationem quasi tempore quisquam, at vero quod harum ad et nulla, ipsum repellat illum, eum magnam laudantium? Ipsum dolorem ipsam corporis?</p>
+    </article>
+
+    <article>
+        <i class="fas fa-hand-holding-usd"></i>
+        <h2>Marketing</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque exercitationem quasi tempore quisquam, at vero quod harum ad et nulla, ipsum repellat illum, eum magnam laudantium? Ipsum dolorem ipsam corporis?</p>
+    </article>
+
+    <article>
+        <i class="fas fa-bullhorn"></i>
+        <h2>Marketing</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque exercitationem quasi tempore quisquam, at vero quod harum ad et nulla, ipsum repellat illum, eum magnam laudantium? Ipsum dolorem ipsam corporis?</p>
+    </article>
+</section>
+
+<!-- three -->
+<section class="three">
+    <div class="oneTwo container">
+
+    <div class="left">
+        <p>03</p>
+    </div>
+    <div class="right">
+        <h5>SERVICES</h5>
+        <h2><span>HOW TO PROVIDE </span>TOOLS THAT HELP ANYONE</h2>
+        <h4>Get your Business in 4 easy steps</h4>
+        <button>About us</button>
+    </div>
+    </div>
+
+    <div class="numbers">
+    <article>
+        <p>18</p>
+        <h3>Nominations</h3>
+    </article>
+
+    <article>
+        <p>5</p>
+        <h3>Awwards</h3>
+    </article>
+
+    <article>
+        <p>45</p>
+        <h3>Agencies</h3>
+    </article>
+    </div>
+</section>
+
+<!-- four -->
+<section class="oneTwo container">
+<div class="left">
+        <p>04</p>
+    </div>
+    <div class="right"> 
+        <h5>OUR TEAM</h5>
+        <h2><span>We Have a Professional </span>Team of Business Analysts.</h2>
+        <h4>Get your Business in 4 easy steps</h4>
+    </div>
+</section>
+
+<!-- team -->
+<section class="team container">
+    <article>
+        <img src="img/team1.jpg" alt="">
+            <h3>James Douglas</h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. A rerum voluptatibus obcaecati quod numquam, libero tempore minima commodi reiciendis doloremque!</p>
+    </article>
+
+    <article>
+        <img src="img/team2.jpg" alt="">
+            <h3>Peter Green</h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. A rerum voluptatibus obcaecati quod numquam, libero tempore minima commodi reiciendis doloremque!</p>
+    </article>
+
+    <article>
+        <img src="img/team3.jpg" alt="">
+            <h3>Etta James</h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. A rerum voluptatibus obcaecati quod numquam, libero tempore minima commodi reiciendis doloremque!</p>
+    </article>
+</section>
+
+<!-- footer -->
+<footer>
+    <div class="container">
+    <article>
+        <div class="log">BUSINESS <br>
+        <p>Marketing & consulting</p>
+    </div>
+    </article>
+    <article>
+        <i class="fas fa-map-marker"></i>
+        <p>Main street 2, Novi Sad</p>
+        <p>Serbia</p>
+    </article>
+    <article>
+        <i class="fas fa-envelope"></i>
+        <p>Main street 2, Novi Sad</p>
+        <p>Serbia</p>
+    </article>
+    <article>
+        <i class="fas fa-phone"></i>
+        <p>Main street 2, Novi Sad</p>
+        <p>Serbia</p>
+    </article>
+    
+</footer>
+
+</body>
+</html>
